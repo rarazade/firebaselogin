672b8a8a51d5cf61e33a7cdca056ed31e092f38c
rarazade <isabellaadr13@gmail.com>
2024-04-23 17:10:43 +0700

    rarafirebase

--- app/src/main/java/com/example/datatemann/MainActivity.kt
+++ app/src/main/java/com/example/datatemann/MainActivity.kt
@@ -83,7 +83,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     })
             }
-            R.id.show_data -> {}
+            R.id.show_data -> {
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
+            }
         }
     }
 }
